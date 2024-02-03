351377952f4d6485336b92b8de4997d7ee509ad1
Joshua Lester <jlester3@ewu.edu>
2024-02-03 13:17:20 -0800

    csproj file + FullName init

---
+++ Logger.Tests/FullNameTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Logger.Tests;
+
+public class FullNameTests
+{
+    [Fact]
+    public void FullNameConstructor_NonNullParams_Success()
+    {
+        FullName fullName = new("Jimmy", "Johns");
+        Assert.True(fullName is FullName);
+    }
+
+    [Fact]
+    public void FullNameConstructor_NonNullParamsWithMiddleName_Success()
+    {
+        FullName fullName = new("Jimmy", "Johns", "Flippin'");
+        Assert.True(fullName is FullName);
+    }
+
+    [Fact]
+    public void FullNameConstructor_NullParams_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new FullName("Jimmy?", null!));
+    }
+}
---
+++ Logger/FullName.cs
@@ -0,0 +1,17 @@
+﻿namespace Logger;
+
+public record class FullName(string firstName, string lastName, string? middleName = null)
+{
+    public string FirstName 
+    { 
+        get => _FirstName; 
+        init => _FirstName = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    private string _FirstName;
+    public string LastName
+    {
+        get => _LastName;
+        init => _LastName = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    private string _LastName;
+}
