b9cd5092349cc0a6f9c72a66d9972d1cc68f2451
rahat15 <rbhatia@ewu.edu>
2024-02-04 02:31:41 -0800

    Person record

---
+++ Logger/Person.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Logger;
+
+//for generalizing code between Employee and Student
+public record Person(Guid Id, FullName FullName) : IEntity
+{
+    public string Name => $"{FullName.FirstName} {FullName.LastName}";
+}
