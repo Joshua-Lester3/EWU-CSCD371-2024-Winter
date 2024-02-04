4885d06b4bffd73618b359a3fdb52d4b0a49e2b4
rahat15 <rbhatia@ewu.edu>
2024-02-04 02:16:02 -0800

    Base Entity update

--- Logger/BaseEntity.cs
+++ Logger/BaseEntity.cs
@@ -1,6 +1,9 @@
-﻿namespace Logger
-{
-    public class BaseEntity
+﻿namespace Logger;
+
+    public abstract class BaseEntity : IEntity
     {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public abstract string Name { get; }
+
+        string IEntity.Name => Name;
     }
-}
--- Logger/IEntity.cs
+++ Logger/IEntity.cs
@@ -1,6 +1,6 @@
 ﻿namespace Logger;
 public interface IEntity
 {
-    public string Name { get; set; }
+    public string Name { get; }
     public Guid Id { get; init; }
 }
