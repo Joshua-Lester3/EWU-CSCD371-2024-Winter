7350df07ea6ecf1cc5b3bf4575b9d649068e9d8f
Joshua Lester <jlester3@ewu.edu>
2024-02-07 17:51:08 -0800

    added tests for storagetests

--- Logger.Tests/StorageTests.cs
+++ Logger.Tests/StorageTests.cs
@@ -6,32 +6,44 @@ namespace Logger.Tests;
 
 public class StorageTests
 {
+    #region private fields
     private readonly Storage _Storage;
     private readonly Employee _Employee1;
     private readonly Employee _Employee2;
+    private readonly Book _Book1;
+    private readonly Book _Book2;
+    private readonly Student _Student1;
+    private readonly Student _Student2;
+    #endregion
     public StorageTests()
     {
         _Storage = new();
-        _Employee1 = new Employee(Guid.NewGuid(), new FullName("Jim", "John"));
-        _Employee2 = new Employee(Guid.NewGuid(), new FullName("Jimmy", "John"));
+        _Employee1 = new(Guid.NewGuid(), new FullName("Jim", "John"));
+        _Employee2 = new(Guid.NewGuid(), new FullName("Jimmy", "John"));
+        _Book1 = new(Guid.NewGuid());
+        _Book2 = new(Guid.NewGuid());
+        _Student1 = new(Guid.NewGuid(), new FullName("Jim", "John"));
+        _Student2 = new(Guid.NewGuid(), new FullName("Jimmy", "John"));
     }
 
+    #region Employee
+
     [Fact]
-    public void Contains_DoesContain_ReturnsTrue()
+    public void EmployeeContains_DoesContain_ReturnsTrue()
     {
         _Storage.Add(_Employee2);
         Assert.True(_Storage.Contains(_Employee2));
     }
 
     [Fact]
-    public void Contains_DoesNotContain_ReturnsFalse()
+    public void EmployeeContains_DoesNotContain_ReturnsFalse()
     {
         _Storage.Add(_Employee2);
         Assert.False(_Storage.Contains(_Employee1));
     }
 
     [Fact]
-    public void Remove_DoesContain_DoesNotContainAnymore()
+    public void EmployeeRemove_DoesContain_DoesNotContainAnymore()
     {
         _Storage.Add(_Employee2);
         Assert.True(_Storage.Contains(_Employee2));
@@ -40,7 +52,7 @@ public class StorageTests
     }
 
     [Fact]
-    public void Add_DoesNotContain_DoesContainNow()
+    public void EmployeeAdd_DoesNotContain_DoesContainNow()
     {
         Assert.False(_Storage.Contains(_Employee1));
         _Storage.Add(_Employee1);
@@ -48,7 +60,7 @@ public class StorageTests
     }
 
     [Fact]
-    public void Get_DoesNotContain_ReturnsNull()
+    public void EmployeeGet_DoesNotContain_ReturnsNull()
     {
         _Storage.Add(_Employee1);
         Assert.True(_Storage.Contains(_Employee1));
@@ -56,9 +68,107 @@ public class StorageTests
     }
 
     [Fact]
-    public void Get_DoesContain_ReturnsEmployee1()
+    public void EmployeeGet_DoesContain_ReturnsEmployee1()
     {
         _Storage.Add(_Employee1);
         Assert.Equal(_Employee1, _Storage.Get(((IEntity)_Employee1).Id));
     }
+    #endregion
+
+    #region Book
+
+    [Fact]
+    public void BookContains_DoesContain_ReturnsTrue()
+    {
+        _Storage.Add(_Book2);
+        Assert.True(_Storage.Contains(_Book2));
+    }
+
+    [Fact]
+    public void BookContains_DoesNotContain_ReturnsFalse()
+    {
+        _Storage.Add(_Book1);
+        Assert.False(_Storage.Contains(_Book2));
+    }
+
+    [Fact]
+    public void BookRemove_DoesContain_DoesNotContainAnymore()
+    {
+        _Storage.Add(_Book2);
+        Assert.True(_Storage.Contains(_Book2));
+        _Storage.Remove(_Book2);
+        Assert.False(_Storage.Contains(_Book2));
+    }
+
+    [Fact]
+    public void BookAdd_DoesNotContain_DoesContainNow()
+    {
+        Assert.False(_Storage.Contains(_Book2));
+        _Storage.Add(_Book2);
+        Assert.True(_Storage.Contains(_Book2));
+    }
+
+    [Fact]
+    public void BookGet_DoesNotContain_ReturnsNull()
+    {
+        _Storage.Add(_Book2);
+        Assert.True(_Storage.Contains(_Book2));
+        Assert.Null(_Storage.Get(((IEntity)_Book1).Id));
+    }
+
+    [Fact]
+    public void BookGet_DoesContain_ReturnsEmployee1()
+    {
+        _Storage.Add(_Book2);
+        Assert.Equal(_Book2, _Storage.Get(((IEntity)_Book2).Id));
+    }
+    #endregion
+
+    #region Student
+    [Fact]
+    public void StudentContains_DoesContain_ReturnsTrue()
+    {
+        _Storage.Add(_Student2);
+        Assert.True(_Storage.Contains(_Student2));
+    }
+
+    [Fact]
+    public void StudentContains_DoesNotContain_ReturnsFalse()
+    {
+        _Storage.Add(_Student1);
+        Assert.False(_Storage.Contains(_Student2));
+    }
+
+    [Fact]
+    public void StudentRemove_DoesContain_DoesNotContainAnymore()
+    {
+        _Storage.Add(_Student2);
+        Assert.True(_Storage.Contains(_Student2));
+        _Storage.Remove(_Student2);
+        Assert.False(_Storage.Contains(_Student2));
+    }
+
+    [Fact]
+    public void StudentAdd_DoesNotContain_DoesContainNow()
+    {
+        Assert.False(_Storage.Contains(_Student2));
+        _Storage.Add(_Student2);
+        Assert.True(_Storage.Contains(_Student2));
+    }
+
+    [Fact]
+    public void StudentGet_DoesNotContain_ReturnsNull()
+    {
+        _Storage.Add(_Student2);
+        Assert.True(_Storage.Contains(_Student2));
+        Assert.Null(_Storage.Get(((IEntity)_Student1).Id));
+    }
+
+    [Fact]
+    public void StudentGet_DoesContain_ReturnsEmployee1()
+    {
+        _Storage.Add(_Student2);
+        Assert.Equal(_Student2, _Storage.Get(((IEntity)_Student2).Id));
+    }
+    #endregion
 }
