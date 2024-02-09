4c1809818e3d1b615f391461ec8d5791f21c075a
Joshua Lester <jlester3@ewu.edu>
2024-02-09 09:57:40 -0800

    created empty solution, class library, tests class

---
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace GenericsHomework.Tests;
+public class NodeTests
+{
+    [Fact]
+    public void Element_NodeInitialized_ReturnsCorrectType()
+    {
+        Node<string> node = new();
+        Assert.True(node.Element is string);
+    }
+}
