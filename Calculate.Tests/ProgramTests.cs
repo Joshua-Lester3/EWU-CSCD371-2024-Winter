7908d8f287a2d6adee6cfe753cc15e24bfa88d36
Joshua Lester <jlester3@ewu.edu>
2024-02-20 18:05:58 -0800

    Fixed tests

--- Calculate.Tests/ProgramTests.cs
+++ Calculate.Tests/ProgramTests.cs
@@ -1,4 +1,6 @@
-﻿namespace Calculate.Tests;
+﻿using IntelliTect.TestTools.Console;
+
+namespace Calculate.Tests;
 
 public class ProgramTests
 {
@@ -7,141 +9,114 @@ public class ProgramTests
     public void Main_Welcome_Success()
     {
         // Arrange
-        TextWriter oldOut = Console.Out;
-        StringWriter newOut = new StringWriter();
-        Console.SetOut(newOut);
-        TextReader oldIn = Console.In;
-        StringReader newIn = new("q" + Environment.NewLine);
-        Console.SetIn(newIn);
+        string view = @"Welcome to the Calculator!
+Please enter a calculation (or 'q' to quit):<<
+>>
+Exiting...";
 
         // Act
-        Program.Main();
-        Console.SetOut(oldOut);
-        Console.SetIn(oldIn);
+
 
         // Assert
-        string expected = "Welcome to the Calculator!" + Environment.NewLine
-            + "Please enter a calculation (or 'q' to quit):" + Environment.NewLine;
-        Assert.Contains(expected, newOut.ToString());
+        ConsoleAssert.Expect(view, Program.Main);
     }
 
     [Fact]
     public void Main_ProgramCalculates_Success()
     {
         // Arrange
-        TextReader oldIn = Console.In;
-        StringReader newIn = new("4 + 2" + Environment.NewLine + "q" + Environment.NewLine);
-        Console.SetIn(newIn);
-        TextWriter oldOut = Console.Out;
-        StringWriter newOut = new StringWriter();
-        Console.SetOut(newOut);
-
+        string view = @"Welcome to the Calculator!
+Please enter a calculation (or 'q' to quit):<<4 + 2
+>>
+What a lovely calculation! The answer... is 6
+Please enter a calculation (or 'q' to quit):<<
+>>
+Exiting...";
         // Act
-        Program.Main();
-        Console.SetIn(oldIn);
-        Console.SetOut(oldOut);
 
         // Assert
-        Assert.Contains("What a lovely calculation! The answer... is 6", newOut.ToString());
+        ConsoleAssert.Expect(view, Program.Main);
     }
 
     [Fact]
     public void Main_ProgramQuit_Success()
     {
         // Arrange
-        TextReader oldIn = Console.In;
-        StringReader newIn = new($"4 + 2{Environment.NewLine}q{Environment.NewLine}");
-        Console.SetIn(newIn);
-        TextWriter oldOut = Console.Out;
-        StringWriter newOut = new StringWriter();
-        Console.SetOut(newOut);
+        string view = @"Welcome to the Calculator!
+Please enter a calculation (or 'q' to quit):<<q
+>>
+Exiting...";
 
         // Act
-        Program.Main();
-        Console.SetIn(oldIn);
-        Console.SetOut(oldOut);
 
         // Assert
-        Assert.Contains("Exiting...", newOut.ToString());
+        ConsoleAssert.Expect(view, Program.Main);
     }
 
     [Fact]
     public void Main_ProgramRepeat_Success()
     {
         // Arrange
-        TextReader oldIn = Console.In;
-        StringReader newIn = new($"4 + 2{Environment.NewLine}5 + 4{Environment.NewLine}q{Environment.NewLine}");
-        Console.SetIn(newIn);
-        using TextWriter oldOut = Console.Out;
-        StringWriter newOut = new StringWriter();
-        Console.SetOut(newOut);
+        string view = @"Welcome to the Calculator!
+Please enter a calculation (or 'q' to quit):<<4 + 2
+>>
+What a lovely calculation! The answer... is 6
+Please enter a calculation (or 'q' to quit):<<6 + 3
+>>
+What a lovely calculation! The answer... is 9
+Please enter a calculation (or 'q' to quit):
+Exiting...";
 
         // Act
-        Program.Main();
-        Console.SetIn(oldIn);
-        Console.SetOut(oldOut);
 
         // Assert
-        Assert.Contains("The answer... is 6", newOut.ToString());
-        Assert.Contains("The answer... is 9", newOut.ToString());
+        ConsoleAssert.Expect(view, Program.Main);
     }
 
     [Theory]
-    [MemberData(nameof(TestCases1))]
-    public void Main_InvalidInput_AsksToTryAgain(string input)
+    [MemberData(nameof(TestCases))]
+    public void Main_InvalidInput_AsksToTryAgain(string view)
     {
         // Arrange
-        TextReader oldIn = Console.In;
-        StringReader newIn = new(input);
-        Console.SetIn(newIn);
-        TextWriter oldOut = Console.Out;
-        StringWriter newOut = new StringWriter();
-        Console.SetOut(newOut);
 
         // Act
-        Program.Main();
-        Console.SetIn(oldIn);
-        Console.SetOut(oldOut);
 
         // Assert
-        Assert.Contains("Invalid input. Please try again.", newOut.ToString());
-        Assert.Contains("Exiting...", newOut.ToString());
+        ConsoleAssert.Expect(view, Program.Main);
     }
 
     // Used for MemberData in Main_InvalidInput_AsksToTryAgain()
-    public static IEnumerable<object[]> TestCases1 { get; } = new object[][]
+    public static IEnumerable<object[]> TestCases { get; } = new object[][]
     {
-        new object[] { $"4 e 4{Environment.NewLine}q" },
-        new object[] { $"eeee{Environment.NewLine}q" }
+        new object[] { @"Welcome to the Calculator!
+Please enter a calculation (or 'q' to quit):<<4eee4
+>>
+Invalid input. Please try again.
+Please enter a calculation (or 'q' to quit):<<
+>>
+Exiting..." },
+        new object[] { @"Welcome to the Calculator!
+Please enter a calculation (or 'q' to quit):<<eee 4
+>>
+Invalid input. Please try again.
+Please enter a calculation (or 'q' to quit):<<
+>>
+Exiting..." }
     };
 
-    [Theory]
-    [MemberData(nameof(TestCases2))]
-    public void Main_NoInput_SuccessfullyQuits(string input)
+    [Fact]
+    public void Main_NoInput_SuccessfullyQuits()
     {
         // Arrange
-        TextReader oldIn = Console.In;
-        StringReader newIn = new(input);
-        Console.SetIn(newIn);
-        TextWriter oldOut = Console.Out;
-        StringWriter newOut = new StringWriter();
-        Console.SetOut(newOut);
+        string view = @"Welcome to the Calculator!
+Please enter a calculation (or 'q' to quit):<<
+>>
+Exiting...";
 
         // Act
-        Program.Main();
-        Console.SetIn(oldIn);
-        Console.SetOut(oldOut);
 
         // Assert
-        Assert.Contains("Exiting...", newOut.ToString());
+        ConsoleAssert.Expect(view, Program.Main);
     }
-
-    // Used for MemberData in Main_NoInput_SuccessfullyQuits()
-    // Used to simulate either no more lines to read or a Ctrl + Z shortcut to end the application
-    public static IEnumerable<object[]> TestCases2 { get; } = new object[][]
-    {
-        new object[] { $"4 + 4{Environment.NewLine}q" },
-        new object[] { $"{Environment.NewLine}q" }
-    };
     #endregion
 }
