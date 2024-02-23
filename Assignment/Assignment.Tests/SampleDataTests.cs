575ab151ca47a218abe07b80df879fae93b5eba3
Joshua Lester <jlester3@ewu.edu>
2024-02-23 07:25:26 -0800

    almost done with req2

--- Assignment/Assignment.Tests/SampleDataTests.cs
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -10,40 +10,105 @@ namespace Assignment.Tests;
 
 public class SampleDataTests
 {
-    public SampleData SampleData { get; init; }
-    public IEnumerable<string> TestCsvRows { get; init; }
-    public SampleDataTests()
+    #region Constructor + Properties Tests
+    [Fact]
+    public void SampleData_InitializedFilePath_Success()
     {
-        SampleData = new();
+        // Arrange
+        SampleData sampleData = new("People.csv");
         // Many null conditional operators and a null forgiving operator because as long as the
         // file structure stays the same, and People.csv stays in the same place, this will be
         // non-null.
-        string directory = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())?.
+        string filePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())?.
             Parent?.Parent?.Parent?.FullName!, "Assignment", "People.csv");
-        TestCsvRows = File.ReadAllLines(directory).
-            Skip(1);
+        IEnumerable<string> expected = File.ReadLines(filePath).Skip(1);
 
+        // Act
+
+        // Assert
+        Assert.Equal(expected, sampleData.CsvRows);
     }
+
+    [Fact]
+    public void SampleData_DefaultFilePath_Success()
+    {
+        // Arrange
+        SampleData sampleData = new();
+        string filePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())?.
+            Parent?.Parent?.Parent?.FullName!, "Assignment", "People.csv");
+        IEnumerable<string> expected = File.ReadLines(filePath).Skip(1);
+
+        // Act
+
+        // Assert
+        Assert.Equal(expected, sampleData.CsvRows);
+    }
+    #endregion
+
+    #region Requirements 1-7 Tests
     [Fact]
     public void CsvRows_Get_Success()
     {
         // Arrange
+        SampleData sampleData = new("People.csv");
+        string filePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())?.
+            Parent?.Parent?.Parent?.FullName!, "Assignment", "People.csv");
+        IEnumerable<string> data = File.ReadAllLines(filePath).Skip(1);
 
         // Act
 
         // Assert
-        Assert.Equal(TestCsvRows, SampleData.CsvRows);
+        Assert.Equal(data, sampleData.CsvRows);
+    }
+
+    //TODO: 1. test uniqueness for req2; 2. test sortedness for req2
+
+    [Fact]
+    public void GetUniqueSortedListOfStatesGivenCsvRows_NormalCondition_SuccessfullyContainsCorrectItems()
+    {
+        // Arrange
+        SampleData sampleData = new("TestingCsv.csv");
+
+        // Act
+        //IEnumerable<string> data = new string[] {
+        //    "AL", "AK", "AZ", "AR",
+        //    "CA","CO","CT","DE","DC","FL","GA","HI",
+        //    "ID","IL","IN","IA","KS","KY","LA","ME", "MD",
+        //    "MA","MI","MN","MS","MO","MT","NE","NV",
+        //    "NH","NJ","NM","NY","NC","ND","OH","OK",
+        //    "OR","PA","RI","SC","SD","TN","TX","UT",
+        //    "VT","VA","WA","WV","WI","WY"
+        //};
+        IEnumerable<string> data = new string[] {
+            "CA", "FL", "GA", "MT"
+        };
+
+
+        // Assert
+        IEnumerable<string> uniqueSortedListOfStates = sampleData.GetUniqueSortedListOfStatesGivenCsvRows();
+        Assert.True(uniqueSortedListOfStates.All(item => data.Contains(item)));
     }
 
     [Fact]
-    public void GetUniqueSortedListOfStatesGivenCsvRows_NormalCondition_Success()
+    public void GetUniqueSortedListOfStatesGivenCsvRows_NormalCondition_SuccessfullySorts()
+    {
+
+    }
+
+    [Fact]
+    public void GetUniqueSortedListOfStatesGivenCsvRows_NormalCondition_ItemsAreUnique()
     {
         // Arrange
+        SampleData sampleData = new("TestingCsv.csv");
 
         // Act
-        IEnumerable<string> data = TestCsvRows;
+        IEnumerable<string> data = new string[] {
+            "CA", "FL", "GA", "MT"
+        };
+
 
         // Assert
-        Assert.Equal(data, SampleData.GetUniqueSortedListOfStatesGivenCsvRows());
+        Assert.True(sampleData.GetUniqueSortedListOfStatesGivenCsvRows().SequenceEqual(data));
     }
+    #endregion
 }
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
-        // 1.
-        public IEnumerable<string> CsvRows => File.ReadLines("People.csv").Skip(1);
+        public string FilePath { get; set; }
+        public SampleData(string filePath)
+        {
+            FilePath = filePath;
+        }
+        public SampleData() : this("People.csv") { }
 
-         
+        // 1.
+        public IEnumerable<string> CsvRows => File.ReadLines(FilePath).Skip(1);
 
         // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => CsvRows;
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+        {
+            return (from line in CsvRows
+                   let elements = line.Split(',')
+                   let state = elements[elements.Length - 2]
+                   orderby state
+                   select state).Distinct();
+        }
 
         // 3.
         public string GetAggregateSortedListOfStatesUsingCsvRows()
