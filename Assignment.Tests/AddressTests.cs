706373e4a4a675127130c38d5e83e35514c75f26
SaaWest <swest6@ewu.edu>
2024-03-01 12:32:20 -0800

    address test

--- Assignment.Tests/AddressTests.cs
+++ Assignment.Tests/AddressTests.cs
@@ -1,8 +1,23 @@
 
-namespace Assignment.Tests
-{
-    public class AddressTests
-    {
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
 
+namespace Assignment.Tests;
+public class AddressTests
+{
+    //These test are not really needed but to check another test in SampleDataTests.csv
+    [Fact]
+    public void CheckAddressConstructor_CSVAddress_Is_The_Same()
+    {
+        var address = new Address("94148 Kings Terrace", "Long Beach", "CA", "59721");
+        SampleData data = new SampleData("TestingCsv.csv");
+        
+        List<string> dataList = data.CsvRows.ToList();
+        string element = dataList[2].Split(',')[4];
+
+        Assert.Equal(element, address.StreetAddress);
     }
 }
+
