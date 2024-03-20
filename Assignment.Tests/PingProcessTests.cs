60cdb2d1fe7178ce1795f7b9ba233cf4eccac0de
Joshua Lester <jlester3@ewu.edu>
2024-03-19 18:23:09 -0700

    adjusting PingOutputLikeExpression for linux runner

--- Assignment.Tests/PingProcessTests.cs
+++ Assignment.Tests/PingProcessTests.cs
@@ -25,7 +25,7 @@ public class PingProcessTests
     [TestMethod]
     public void Start_PingProcess_Success()
     {
-        Process process = Process.Start("ping -c 4", "localhost");
+        Process process = Process.Start("ping", "-c 4 localhost");
         process.WaitForExit();
         Assert.AreEqual<int>(0, process.ExitCode);
     }
@@ -212,17 +212,26 @@ public class PingProcessTests
 
     }
 
-    readonly string PingOutputLikeExpression = @"
-Pinging * with 32 bytes of data:
-Reply from ::1: time<*
-Reply from ::1: time<*
-Reply from ::1: time<*
-Reply from ::1: time<*
+//    readonly string PingOutputLikeExpression = @"
+//Pinging * with 32 bytes of data:
+//Reply from ::1: time<*
+//Reply from ::1: time<*
+//Reply from ::1: time<*
+//Reply from ::1: time<*
 
-Ping statistics for ::1:
-    Packets: Sent = *, Received = *, Lost = 0 (0% loss),
-Approximate round trip times in milli-seconds:
-    Minimum = *, Maximum = *, Average = *".Trim();
+//Ping statistics for ::1:
+//    Packets: Sent = *, Received = *, Lost = 0 (0% loss),
+//Approximate round trip times in milli-seconds:
+//    Minimum = *, Maximum = *, Average = *".Trim();
+
+    readonly string PingOutputLikeExpression = @"PING * 56 data bytes
+64 bytes from localhost (::1): icmp_seq=1 ttl=64 time=* ms
+64 bytes from localhost (::1): icmp_seq=2 ttl=64 time=* ms
+64 bytes from localhost (::1): icmp_seq=3 ttl=64 time=* ms
+64 bytes from localhost (::1): icmp_seq=4 ttl=64 time=* ms
+--- localhost ping statistics ---
+4 packets transmitted, 4 received, 0% packet loss, time *ms
+rtt min/avg/max/mdev = */*/*/* ms".Trim();
     private void AssertValidPingOutput(int exitCode, string? stdOutput)
     {
         Assert.IsFalse(string.IsNullOrWhiteSpace(stdOutput));
